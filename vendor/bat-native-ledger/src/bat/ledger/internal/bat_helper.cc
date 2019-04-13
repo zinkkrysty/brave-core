d8ec237cfed6b25d3fc8b26c95bb54b7e3a4a775
George Dregalo <37560650+gdregalo@users.noreply.github.com>
2019-04-13 16:58:55 -0400

    fixed WALLET_PROPERTIES_ST serialization

--- vendor/bat-native-ledger/src/bat/ledger/internal/bat_helper.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/bat_helper.cc
@@ -863,6 +863,7 @@ void saveToJson(JsonWriter* writer, const WALLET_PROPERTIES_ST& data) {
   for (auto & choice : data.parameters_choices_) {
     writer->Double(choice);
   }
+
   writer->EndArray();
   writer->EndObject();
 
