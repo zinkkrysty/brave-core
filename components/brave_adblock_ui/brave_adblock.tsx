4b95ef0a0ea2309a61855ad4cd357682c67f8454
Cezar Augusto <boss@cezaraugusto.net>
2019-10-28 18:28:13 -0300

    start playlist interface

--- components/brave_adblock_ui/brave_adblock.tsx
+++ components/brave_adblock_ui/brave_adblock.tsx
@@ -36,43 +36,6 @@ window.cr.define('brave_adblock', function () {
       </Provider>,
       document.getElementById('root'))
     window.i18nTemplate.process(window.document, window.loadTimeData)
-
-    // playlist API demo
-
-    // initialize Brave Playlists API
-    chrome.bravePlaylists.initialize()
-
-    chrome.bravePlaylists.isInitialized((init) => {
-      if (init) {
-        return
-      }
-
-      // wait for API initialization to complete before continuing
-      chrome.bravePlaylists.onInitialized.addListener(() => {
-
-        // listen for Playlist API events
-        chrome.bravePlaylists.onPlaylistsChanged.addListener( (changeType, id) => {
-          console.log(" ##### " + changeType + " " + id + " #####")
-        })
-
-        // request a download (will create a playlist if successful)
-        chrome.bravePlaylists.requestDownload("https://www.youtube.com/watch?v=k8HoGzD4mWw")
-
-//        // make a playlist with a short stock video
-//        chrome.bravePlaylists.createPlaylist(
-//          {
-//            thumbnailUrl: "https://www.cutestockfootage.com/content/10501/thumb100.jpg",
-//            playlistName: "Light Leaks Element 423",
-//            mediaFiles: [
-//              {
-//                url: "https://www.cutestockfootage.com/content/10501/preview.mp4",
-//                title: "Light Leaks Element 423"
-//              }
-//            ]
-//          }
-//        )
-      })
-    })
   }
 
   function onGetCustomFilters (customFilters: string) {
--- components/brave_adblock_ui/components/app.tsx
+++ components/brave_adblock_ui/components/app.tsx
@@ -14,6 +14,9 @@ import { NumBlockedStat } from './numBlockedStat'
 // Utils
 import * as adblockActions from '../actions/adblock_actions'
 
+// TODO cezaraugusto methods used here should exported and not bound to the window obj
+import '../vendor/youtubedown'
+
 interface Props {
   actions: any
   adblockData: AdBlock.State
@@ -28,10 +31,104 @@ export class AdblockPage extends React.Component<Props, {}> {
     return this.props.actions
   }
 
+  get video () {
+    return {
+      muffinTime: 'https://www.youtube.com/watch?v=LACbVhgtx9I',
+      uShouldBeDancing: 'https://www.youtube.com/watch?v=xI68A-rntIk',
+      babyShark: 'https://www.youtube.com/watch?v=020g-0hhCAU',
+      paleBlueDot: 'https://www.youtube.com/watch?v=wupToqz1e2g'
+    }
+  }
+
+  componentDidMount () {
+    chrome.bravePlaylists.initialize()
+
+    chrome.bravePlaylists.isInitialized((init) => {
+      if (init) {
+        return
+      }
+      chrome.bravePlaylists.onInitialized.addListener(() => {
+        console.log(' ###### playlist initialized ######')
+
+        // chrome.bravePlaylists.deleteAllPlaylists((deleted: boolean) => {
+        //   console.log(' ##### all playlists deleted #####')
+        // })
+
+        chrome.bravePlaylists.onDownloadRequested.addListener((url) => {
+          console.log(' ##### playlist download requested: ' + url + ' #####')
+          const ytdItems: chrome.bravePlaylists.YTDMediaItem[] = window.youtubedown_urls(url)
+          if (ytdItems.length == 0) {
+            console.log(' ##### got nothing from youtubedown_urls #####')
+            return
+          }
+          const videoMediaFiles: chrome.bravePlaylists.CreateParamsMediaItem[] = this.makeMediaItems(ytdItems[0])
+          let audioMediaFiles: chrome.bravePlaylists.CreateParamsMediaItem[] = []
+          if (ytdItems.length > 1) {
+            // Video is only available as separate video and audio files.
+            // First item is always the video. Second item is always the audio.
+            audioMediaFiles = this.makeMediaItems(ytdItems[1])
+          }
+          console.log(JSON.stringify(videoMediaFiles))
+          console.log(JSON.stringify(audioMediaFiles))
+          chrome.bravePlaylists.createPlaylist(
+            {
+              thumbnailUrl: '',
+              playlistName: 'Untitled-1',
+              videoMediaFiles,
+              audioMediaFiles
+            }
+          )
+        })
+      })
+    })
+  }
+
+  requestVideoDownload = (video: string) => {
+    chrome.bravePlaylists.requestDownload(video)
+  }
+
+  makeMediaItems = (item: chrome.bravePlaylists.YTDMediaItem) => {
+    let mediaFiles: chrome.bravePlaylists.CreateParamsMediaItem[]
+    if (typeof item.url === 'string') {
+      // Video is all in one file, but we'll pretend it's an array of 1 segment
+      mediaFiles = [{ url: item.url, title: item.file }]
+    } else {
+      // Video is split into segments, which we will concatenate later
+      const urls: string[] = item.url
+      mediaFiles = urls.map(
+        segment => {
+          return { url: segment, title: item.file }
+        })
+    }
+    return mediaFiles
+  }
+
   render () {
     const { actions, adblockData } = this.props
     return (
       <div id='adblockPage'>
+        <h1>Hello this is the Brave Playlists feature</h1>
+        <ol>
+          <li>
+            <div style={{ display: 'none' }}>first video</div>
+            <span>Muffin Time</span>
+            <button
+              onClick={this.requestVideoDownload.bind(this, this.video.muffinTime)}
+            >
+              download me
+            </button>
+          </li>
+          <li>
+            <div>second video</div>
+          </li>
+          <li>
+            <div>third video</div>
+          </li>
+          <li>
+            <div>fourth video</div>
+          </li>
+        </ol>
+        <hr />
         <NumBlockedStat adsBlockedStat={adblockData.stats.adsBlockedStat || 0} />
         <AdBlockItemList
           actions={actions}
