866654e4353a5761e07c2c8a72abb94c184ad87a
Simon Hong <shong@brave.com>
2020-07-16 11:54:52 +0900

    Fix browser test build failure

--- browser/extensions/brave_playlists_apitest.cc
+++ browser/extensions/brave_playlists_apitest.cc
@@ -9,6 +9,7 @@
 #include "brave/common/extensions/extension_constants.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "components/network_session_configurator/common/network_switches.h"
+#include "content/public/test/browser_test.h"
 #include "extensions/test/result_catcher.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/http_request.h"
