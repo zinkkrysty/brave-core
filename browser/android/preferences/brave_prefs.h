fd2a354e44a489b195cd497758574f1bd6be9248
AlexeyBarabash <alexey@brave.com>
2020-07-21 00:14:24 +0300

    Pull kSyncDeprecationWarningNoticeDismissed pref into Java

--- browser/android/preferences/brave_prefs.h
+++ browser/android/preferences/brave_prefs.h
@@ -8,6 +8,7 @@
 #include <cstddef>
 
 #include "brave/common/pref_names.h"
+#include "brave/components/brave_sync/brave_sync_prefs.h"
 #include "brave/components/ntp_background_images/common/pref_names.h"
 #include "build/build_config.h"
 
@@ -20,6 +21,7 @@ enum BravePref {
   NTP_SHOW_BACKGROUND_IMAGE,
   TOP_SITE_SUGGESTIONS_ENABLED,
   BRAVE_SUGGESTED_SITE_SUGGESTIONS_ENABLED,
+  SYNC_DEPRECATION_WARNING_NOTICE_DISMISSED,
   // BRAVE_PREF_NUM_PREFS must be the last entry.
   BRAVE_PREF_NUM_PREFS
 };
@@ -30,7 +32,8 @@ const char* const kBravePrefsExposedToJava[] = {
     ntp_background_images::prefs::kNewTabPageSuperReferralThemesOption,
     ntp_background_images::prefs::kNewTabPageShowBackgroundImage,
     kTopSiteSuggestionsEnabled,
-    kBraveSuggestedSiteSuggestionsEnabled
+    kBraveSuggestedSiteSuggestionsEnabled,
+    brave_sync::prefs::kSyncDeprecationWarningNoticeDismissed
 };
 
 static const int kBravePrefOffset = 10000;
