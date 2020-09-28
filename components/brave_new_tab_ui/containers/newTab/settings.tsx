382c8b58aaf6a1e3a1206ed55d896eb4573ab157
ryanml <rlanese@brave.com>
2020-09-27 21:49:08 -0700

    Adding crypto.com to NTP dashboard settings

--- components/brave_new_tab_ui/containers/newTab/settings.tsx
+++ components/brave_new_tab_ui/containers/newTab/settings.tsx
@@ -69,6 +69,8 @@ export interface Props {
   togetherSupported: boolean
   showGemini: boolean
   geminiSupported: boolean
+  showCryptoDotCom: boolean
+  cryptoDotComSupported: boolean
   focusMoreCards: boolean
   showBitcoinDotCom: boolean
   bitcoinDotComSupported: boolean
@@ -203,7 +205,10 @@ export default class Settings extends React.PureComponent<Props, State> {
       showGemini,
       toggleShowBitcoinDotCom,
       showBitcoinDotCom,
-      bitcoinDotComSupported
+      bitcoinDotComSupported,
+      toggleShowCryptoDotCom,
+      cryptoDotComSupported,
+      showCryptoDotCom
     } = this.props
     const { activeTab } = this.state
 
@@ -307,6 +312,9 @@ export default class Settings extends React.PureComponent<Props, State> {
                         bitcoinDotComSupported={bitcoinDotComSupported}
                         showBitcoinDotCom={showBitcoinDotCom}
                         toggleShowBitcoinDotCom={toggleShowBitcoinDotCom}
+                        toggleShowCryptoDotCom={toggleShowCryptoDotCom}
+                        cryptoDotComSupported={cryptoDotComSupported}
+                        showCryptoDotCom={showCryptoDotCom}
                       />
                     ) : null
                 }
