57db4dc326529f20e0e863b08395f54c8152ca2e
ryanml <rlanese@brave.com>
2020-09-29 23:02:54 -0700

    Removing together from default stack on nightly

--- browser/brave_profile_prefs.cc
+++ browser/brave_profile_prefs.cc
@@ -242,7 +242,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(kNewTabPageShowStats, true);
   registry->RegisterBooleanPref(kNewTabPageShowRewards, true);
   registry->RegisterBooleanPref(kNewTabPageShowBinance, true);
-  registry->RegisterBooleanPref(kNewTabPageShowTogether, true);
+  registry->RegisterBooleanPref(kNewTabPageShowTogether, false);
   registry->RegisterBooleanPref(kNewTabPageShowAddCard, true);
   registry->RegisterBooleanPref(kNewTabPageShowGemini, true);
 
