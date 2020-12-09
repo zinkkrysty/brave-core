1999cb09b7b82599e2dc42f051ef65874cd5041b
mkarolin <max@brave.com>
2020-12-09 18:44:12 -0500

    Use chromium_src override instead of a patch.

--- chromium_src/extensions/browser/extension_util.cc
+++ chromium_src/extensions/browser/extension_util.cc
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include "extensions/browser/extension_util.h"
 #include "extensions/common/manifest_handlers/incognito_info.h"
 
 #define BRAVE_IS_INCOGNITO_ENABLED \
