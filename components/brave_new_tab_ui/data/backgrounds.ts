0df701c16e2937972986e245c81a2fc94a85c93b
Simon Hong <shong@brave.com>
2021-01-06 09:44:23 +0900

    Revert "Use brave community link for invalid NTP bg images credit link"

--- components/brave_new_tab_ui/data/backgrounds.ts
+++ components/brave_new_tab_ui/data/backgrounds.ts
@@ -23,7 +23,7 @@ export const images: NewTab.Image[] = [
     'name': 'ntp-2020/2021-3',
     'source': 'corwin-prescott_olympic.avif',
     'author': 'Corwin Prescott',
-    'link': 'https://community.brave.com/',
+    'link': '',
     'originalUrl': 'Contributor sent the hi-res version through email',
     'license': 'used with permission'
   },
@@ -63,7 +63,7 @@ export const images: NewTab.Image[] = [
     'name': 'ntp-2020/2021-8',
     'source': 'zane-lee.avif',
     'author': 'Zane Lee',
-    'link': 'https://community.brave.com/',
+    'link': 'https://unsplash.com/@zane4004',
     'originalUrl': 'Contributor sent the hi-res version through email',
     'license': 'https://unsplash.com/photos/ZiDa9i7dY1E'
   }
